362636b7726c46700068ff7a9c8f3a9689fd9393
Salman <s2018065001@umt.edu.pk>
2021-08-02 16:27:26 +0500

    fix https redirects

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/','App\Http\Controllers\MoviesController@index')->httpsOnly()->name('movies.index');
+Route::get('/','App\Http\Controllers\MoviesController@index')->name('movies.index');
 Route::get('/movie/{id}','App\Http\Controllers\MoviesController@show')->httpsOnly()->name('movies.show');
 
 Route::get('/tv','App\Http\Controllers\TvController@index')->httpsOnly()->name('tv.index');
