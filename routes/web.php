22176e7468b764317900e73369ea31806c56f5cc
Salman <s2018065001@umt.edu.pk>
2021-08-02 16:20:19 +0500

    fix https redirects

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/','App\Http\Controllers\MoviesController@index')->name('movies.index');
-Route::get('/movie/{id}','App\Http\Controllers\MoviesController@show')->name('movies.show');
+Route::get('/','App\Http\Controllers\MoviesController@index')->httpsOnly()->name('movies.index');
+Route::get('/movie/{id}','App\Http\Controllers\MoviesController@show')->httpsOnly()->name('movies.show');
 
-Route::get('/tv','App\Http\Controllers\TvController@index')->name('tv.index');
-Route::get('/tv/{id}','App\Http\Controllers\TvController@show')->name('tv.show');
+Route::get('/tv','App\Http\Controllers\TvController@index')->httpsOnly()->name('tv.index');
+Route::get('/tv/{id}','App\Http\Controllers\TvController@show')->httpsOnly()->name('tv.show');
 
-Route::get('/actors','App\Http\Controllers\ActorsController@index')->name('actors.index');
-Route::get('/actors/page/{page?}','App\Http\Controllers\ActorsController@index');
-Route::get('/actor/{id}','App\Http\Controllers\ActorsController@show')->name('actors.show');
+Route::get('/actors','App\Http\Controllers\ActorsController@index')->httpsOnly()->name('actors.index');
+Route::get('/actors/page/{page?}','App\Http\Controllers\ActorsController@index')->httpsOnly();
+Route::get('/actor/{id}','App\Http\Controllers\ActorsController@show')->httpsOnly()->name('actors.show');
